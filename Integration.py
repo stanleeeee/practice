4b907878fd95b04172231b4d8f1d2d4e025ff091
stanleeeee <stanleyslollipop@gmail.com>
2021-11-23 22:58:06 +0000

    trapezium rules

---
+++ Integration.py
@@ -0,0 +1,26 @@
+import math
+
+def get_function():
+    func_value = input("Please input the function in the form of ax: ")
+    func_list = func_value.split(" ")
+    print(func_list)
+    return(func_list)
+
+true_func_list = get_function()
+actual_function = []
+signs = []
+for i in range(len(true_func_list)):
+    if i % 2 == 0:
+        actual_function.appendtrue_func_list[i]
+        print(actual_function)
+    else:
+        signs = true_func_list[i]
+
+range_0 = int(input("Please input range 0: "))
+range_n = int(input("Please input range n: "))
+intervals = int(input("Please input the NUMBER of intervals: "))
+
+    
+        
+        
+    
